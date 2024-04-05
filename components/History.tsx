c7028cc76b71cb96403e3f054094453ef947e2f1
Kingdom Strategies <developer@kingdomstrategies.co>
2024-04-04 18:16:32 -0700

    separating history item, adding swipe to remove

--- components/History.tsx
+++ components/History.tsx
@@ -1,21 +1,19 @@
-import { FontAwesome5, Octicons } from '@expo/vector-icons'
+import { FontAwesome5 } from '@expo/vector-icons'
 import { FlashList } from '@shopify/flash-list'
 import React from 'react'
-import { Dimensions, Text, TouchableOpacity, View } from 'react-native'
+import { Dimensions, Text, View } from 'react-native'
 import Animated, {
-  FadeOutLeft,
   SharedValue,
   useAnimatedStyle,
 } from 'react-native-reanimated'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Spacer from '../Spacer'
 import { colors, gutterSize, type } from '../constants'
-import chapters from '../data/chapters.json'
 import { Chapters } from '../data/types/chapters'
-import { getReference } from '../functions/bible'
-import { HistoryItem, clearHistory } from '../redux/history'
+import { HistoryItem } from '../redux/history'
 import { useAppDispatch, useAppSelector } from '../redux/hooks'
 import Fade from './Fade'
+import HistoryListItem from './HistoryItem'
 
 interface Props {
   activeChapter: Chapters[number]
@@ -37,6 +35,7 @@ export default function History({
   const history = useAppSelector((state) => state.history)
   const insets = useSafeAreaInsets()
   const dispatch = useAppDispatch()
+
   const historyAnimatedStyles = useAnimatedStyle(() => {
     return {
       transform: [{ translateX: textTranslationX.value }],
@@ -50,50 +49,13 @@ export default function History({
     item: HistoryItem
     index: number
   }) {
-    const chapterIndex = (chapters as Chapters).findIndex(
-      (chapter) => chapter.chapterId === item.chapterId
-    )
     return (
-      <Animated.View exiting={FadeOutLeft.duration(100).delay(index * 25)}>
-        <TouchableOpacity
-          onPress={() => {
-            goToChapter(item.chapterId, item.verseIndex)
-
-            closeHistory()
-          }}
-          style={{
-            borderColor: colors.bg3,
-            paddingVertical: 12,
-            backgroundColor:
-              chapterIndex === activeChapterIndex.index
-                ? colors.bg3
-                : undefined,
-            borderTopRightRadius:
-              chapterIndex === activeChapterIndex.index ? 12 : 0,
-            borderBottomRightRadius:
-              chapterIndex === activeChapterIndex.index ? 12 : 0,
-            borderRadius: 12,
-            alignItems: 'center',
-            flexDirection: 'row',
-            paddingHorizontal:
-              chapterIndex === activeChapterIndex.index
-                ? gutterSize / 2
-                : gutterSize / 2,
-            gap: gutterSize / 2,
-          }}
-        >
-          <Text
-            numberOfLines={1}
-            adjustsFontSizeToFit
-            style={[type(18, 'uir', 'l', colors.fg2), { flex: 1 }]}
-          >
-            {getReference(item.chapterId)}:{item.verseIndex + 1}
-          </Text>
-          {chapterIndex === activeChapterIndex.index ? (
-            <Text style={type(12, 'uir', 'c', colors.fg3)}>Current</Text>
-          ) : null}
-        </TouchableOpacity>
-      </Animated.View>
+      <HistoryListItem
+        closeHistory={closeHistory}
+        goToChapter={goToChapter}
+        index={index}
+        item={item}
+      />
     )
   }
 
@@ -134,7 +96,7 @@ export default function History({
             History
           </Text>
         </View>
-        <TouchableOpacity
+        {/* <TouchableOpacity
           onPress={() => dispatch(clearHistory())}
           style={{
             paddingHorizontal: gutterSize / 2,
@@ -149,7 +111,7 @@ export default function History({
         >
           <Octicons name="trash" color={colors.fg3} size={14} />
           <Text style={type(13, 'uir', 'c', colors.fg3)}>Clear</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </View>
       <View style={{ flex: 1 }}>
         <FlashList
@@ -158,8 +120,17 @@ export default function History({
           )}
           contentContainerStyle={{ paddingHorizontal: gutterSize / 2 }}
           renderItem={renderHistoryItem}
+          keyExtractor={(item) => item.chapterId}
           showsVerticalScrollIndicator={false}
           estimatedItemSize={28}
+          ListEmptyComponent={
+            <View style={{ paddingHorizontal: gutterSize / 2 }}>
+              <Text style={type(16, 'uir', 'l', colors.fg3)}>
+                Come back here to return to chapters you were previously
+                reading.
+              </Text>
+            </View>
+          }
           // ListHeaderComponent={
           //   <>
           //     <Spacer units={4} />
@@ -169,7 +140,7 @@ export default function History({
           //     })}
           //   </>
           // }
-          ListHeaderComponent={<Spacer units={2} />}
+          ListHeaderComponent={<Spacer units={4} />}
           ListFooterComponent={<Spacer units={4} additional={insets.bottom} />}
         />
         <Fade place="top" color={colors.bg2} />
---
+++ components/HistoryItem.tsx
@@ -0,0 +1,145 @@
+import { ImpactFeedbackStyle, impactAsync } from 'expo-haptics'
+import React, { useRef } from 'react'
+import { Pressable, Text } from 'react-native'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler'
+import Animated, {
+  FadeOut,
+  interpolate,
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { colors, gutterSize, type } from '../constants'
+import chapters from '../data/chapters.json'
+import { Chapters } from '../data/types/chapters'
+import { getReference } from '../functions/bible'
+import { HistoryItem, removeFromHistory } from '../redux/history'
+import { useAppDispatch, useAppSelector } from '../redux/hooks'
+
+interface Props {
+  item: HistoryItem
+  index: number
+  closeHistory: () => void
+  goToChapter: (
+    chapterId: Chapters[number]['chapterId'],
+    verseNumber?: number
+  ) => void
+}
+
+const swipeReq = 75
+
+export default function HistoryListItem({
+  closeHistory,
+  index,
+  item,
+  goToChapter,
+}: Props) {
+  const activeChapterIndex = useAppSelector((state) => state.activeChapterIndex)
+  const history = useAppSelector((state) => state.history)
+  const insets = useSafeAreaInsets()
+  const dispatch = useAppDispatch()
+  const itemTranslateX = useSharedValue(0)
+  const alreadyHaptic = useRef(false)
+  const pressed = useSharedValue(0)
+  const chapterIndex = (chapters as Chapters).findIndex(
+    (chapter) => chapter.chapterId === item.chapterId
+  )
+
+  function removeHistoryItem() {
+    setTimeout(() => dispatch(removeFromHistory(item.chapterId)), 100)
+  }
+
+  const panGesture = Gesture.Pan()
+    .onChange((event) => {
+      itemTranslateX.value = event.translationX
+
+      if (event.translationX > swipeReq && !alreadyHaptic.current) {
+        runOnJS(impactAsync)(ImpactFeedbackStyle.Heavy)
+        alreadyHaptic.current = true
+      } else if (
+        event.translationX > 0 &&
+        event.translationX < swipeReq &&
+        alreadyHaptic.current === true
+      ) {
+        runOnJS(impactAsync)(ImpactFeedbackStyle.Light)
+        alreadyHaptic.current = false
+      }
+    })
+    .onFinalize((event) => {
+      console.log(event.translationX)
+      if (event.translationX > swipeReq) {
+        itemTranslateX.value = withSpring(swipeReq * 4)
+        runOnJS(removeHistoryItem)()
+      } else {
+        itemTranslateX.value = withSpring(0)
+      }
+    })
+
+  const historyItemStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: itemTranslateX.value }],
+    }
+  })
+
+  const textStyles = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(itemTranslateX.value, [0, swipeReq], [1, 0.5]),
+      textDecorationLine:
+        itemTranslateX.value > swipeReq ? 'line-through' : 'none',
+      // transform: [{ scale: interpolate(pressed.value, [0, 1], [1, 0.5]) }],
+    }
+  })
+
+  return (
+    <GestureDetector gesture={panGesture}>
+      <Animated.View
+        style={historyItemStyles}
+        exiting={FadeOut.duration(100).delay(index * 25)}
+      >
+        <Pressable
+          onPress={() => {
+            // pressed.value = withSequence(
+            //   withTiming(1, { duration: 100 }),
+            //   withTiming(0, {
+            //     duration: 100,
+            //   })
+            // )
+            impactAsync(ImpactFeedbackStyle.Medium)
+            goToChapter(item.chapterId, item.verseIndex)
+
+            closeHistory()
+          }}
+          style={{
+            borderColor: colors.bg3,
+            paddingVertical: 12,
+            backgroundColor:
+              chapterIndex === activeChapterIndex.index
+                ? colors.bg3
+                : undefined,
+            // alignItems: 'center',
+            // flexDirection: 'row',
+            alignItems: 'flex-start',
+            paddingHorizontal:
+              chapterIndex === activeChapterIndex.index
+                ? gutterSize / 2
+                : gutterSize / 2,
+            gap: gutterSize / 2,
+          }}
+        >
+          <Animated.Text
+            numberOfLines={1}
+            adjustsFontSizeToFit
+            style={[type(18, 'uir', 'l', colors.fg2), textStyles]}
+          >
+            {getReference(item.chapterId)}:{item.verseIndex + 1}
+          </Animated.Text>
+          {chapterIndex === activeChapterIndex.index ? (
+            <Text style={type(12, 'uir', 'c', colors.fg3)}>Current</Text>
+          ) : null}
+        </Pressable>
+      </Animated.View>
+    </GestureDetector>
+  )
+}
