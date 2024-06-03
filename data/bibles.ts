9ddd97c4a6d423b849795e337a4e15092d417ae4
Trent Cowden <trent.cowden@gmail.com>
2024-06-03 15:52:36 -0700

    tutorial adjustments

--- data/bibles.ts
+++ data/bibles.ts
@@ -2,12 +2,12 @@ import { Chapters } from './types/chapters'
 import web from './web_chapters.json'
 
 const tutorialMd = [
-  '[1] tov is a simple, thoughtfully designed Bible app made to help you enjoy and study Scripture.',
-  "[2] Here's what you need to know to get started.",
-  '[3] ➡️ Swipe right to open your reading history.',
-  '[4] ✴️✴️ Double tap anywhere to view the books of the Bible and find a specific chapter.',
-  '[5] 👈 Tap on an underlined verse number to view its cross references.',
-  '[6] ⬇️ Keep scrolling downwards to go to the next chapter. Happy reading!',
+  '[1] Inspired by the Hebrew word for "good," **tov** is a delightfully simple yet powerful Bible app designed to help you enjoy and study Scripture.',
+  "[2] Here's what you need to know:",
+  '[3] ➡️ **Swipe right** to open your reading history.',
+  '[4] ✴️✴️ **Double tap** anywhere to view the books of the Bible and find a specific chapter.',
+  '[5] 👈 **Tap on an underlined verse number** to view its cross references.',
+  '[6] ⬇️ **Keep scrolling** downwards to go to the next chapter. Happy reading!',
 ]
 
 const tutorial: Chapters[number] = {
