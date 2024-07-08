26e977c279e40b585c4fb3a073367ad943f96bad
Trent Cowden <trent.cowden@gmail.com>
2024-07-07 17:21:32 -0700

    removing jank splash

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import Aptabase from '@aptabase/react-native'
 import * as Sentry from '@sentry/react-native'
-import Constants from 'expo-constants'
-import * as SplashScreen from 'expo-splash-screen'
 import React from 'react'
 import { View } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
@@ -9,14 +7,8 @@ import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Provider as ReduxProvider } from 'react-redux'
 import { PersistGate as ReduxPersistProvider } from 'redux-persist/lib/integration/react'
 import Bible from './Bible'
-import AnimatedAppLoader from './components/AnimatedSplashScreen'
 import useSessionTracker from './hooks/useSessionTracker'
 import { persistor, store } from './redux/store'
-
-// Instruct SplashScreen not to hide yet, we want to do this manually
-SplashScreen.preventAutoHideAsync().catch(() => {
-  /* reloading the app might trigger some race conditions, ignore them */
-})
 
 Aptabase.init('A-US-7955042107')
 
@@ -32,11 +24,7 @@ function App() {
       <ReduxProvider store={store}>
         <ReduxPersistProvider loading={<View />} persistor={persistor}>
           <GestureHandlerRootView style={{ flex: 1 }}>
-            <AnimatedAppLoader
-              image={{ uri: Constants.expoConfig?.splash?.image }}
-            >
-              <Bible />
-            </AnimatedAppLoader>
+            <Bible />
           </GestureHandlerRootView>
         </ReduxPersistProvider>
       </ReduxProvider>
