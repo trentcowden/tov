8a4b515a3ae8f42faa3d7e7c96e1d788e10434b8
Trent Cowden <trent.cowden@gmail.com>
2024-08-11 20:42:27 -0700

    updating email address

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -189,14 +189,14 @@ export default function Settings({
                   {
                     text: 'Open mail app',
                     onPress: () => {
-                      Linking.openURL('mailto:trent.cowden@gmail.com')
+                      Linking.openURL('mailto:trentcowdenapps@proton.me')
                       trackEvent('Contact: open mail app')
                     },
                   },
                   {
                     text: 'Copy email to clipboard',
                     onPress: () => {
-                      Clipboard.setString('trent.cowden@gmail.com')
+                      Clipboard.setString('trentcowdenapps@proton.me')
                       trackEvent('Contact: copy email to clipboard')
                     },
                   },
